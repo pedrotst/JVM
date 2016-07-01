4e7ab93367591882eb7aa5d6b7e86433936e3153
mashiro11 <thiagoaas1990@gmail.com>
2016-07-01 15:50:28 -0300

    ainda na ldc, sem muito sucesso

--- test/Puppy.java
+++ test/Puppy.java
@@ -2,9 +2,9 @@ public class Puppy{
 
    private int puppyAge;
 
-   public Puppy(String name){
+   public Puppy(/*String name*/){
       // This constructor has one parameter, name.
-      System.out.println("Name chosen is :" + name );
+      //System.out.println("Name chosen is :" + name );
    }
 
    public void setAge( int age ){
@@ -12,13 +12,13 @@ public class Puppy{
    }
 
    public int getAge( ){
-       System.out.println("Puppy's age is :" + puppyAge );
+       //System.out.println("Puppy's age is :" + puppyAge );
        return puppyAge;
    }
 
    public static void main(String []args){
       /* Object creation */
-      Puppy myPuppy = new Puppy( "tommy" );
+      Puppy myPuppy = new Puppy(/* "tommy" */);
 
       /* Call class method to set puppy's age */
       myPuppy.setAge( 2 );
@@ -27,6 +27,6 @@ public class Puppy{
       myPuppy.getAge( );
 
       /* You can access instance variable as follows as well */
-      System.out.println("Variable Value :" + myPuppy.puppyAge );
+      //System.out.println("Variable Value :" + myPuppy.puppyAge );
    }
 }
