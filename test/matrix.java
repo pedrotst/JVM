9c32b0310ca0cbb1093d7cb36f24711868fdc60b
pedroabreu <abreu223@hotmail.com>
2016-07-09 22:47:18 -0300

    matrizes

---
+++ test/matrix.java
@@ -0,0 +1,7 @@
+class matrix{
+	public static void main(String[] args){
+		boolean[] m = {true, true, true, true, false, false, false, false, false};
+
+		System.out.println((m[0] & m[4]) == true);
+	}
+}
